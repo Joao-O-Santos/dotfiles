f3b6596da8748f870eee22ae4ee5516ca74b4473
Joao-O-Santos <joao.filipe.santos@campus.ul.pt>
2022-04-04 20:53:08 +0100

    Refactor backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,3 +1,5 @@
 # SPDX-License-Identifier: CC0-1.0
-su -c "cd $HOME; tar --exclude-from=exclude --acls --xattrs -pvcf bp.tar /"
-xz -T0 -9 $HOME/bp.tar
+su -c "cd $HOME; \
+       tar --exclude-from=$XDG_CONFIG_HOME/scripts/exclude --exclude=$HOME/bp.tar \
+           --acls --xattrs -pvcf bp.tar /; \
+       xz -T0 -9 $HOME/bp.tar"
