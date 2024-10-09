4dc35d03c1c24bf0e85772f124068c9cd5c05c45
Joao-O-Santos <joao.filip.oliv.santos94@gmail.com>
2024-10-09 21:38:32 +0100

    Update unsafest user.js

--- firefox/unsafest/user.js
+++ firefox/unsafest/user.js
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: CC0-1.0
-user_pref("privacy.sanitize.sanitizeOnShutdown", true)
+user_pref("privacy.sanitize.sanitizeOnShutdown", false)
 user_pref("privacy.clearOnShutdown.history", true)
 user_pref("privacy.clearOnShutdown.downloads", true)
-user_pref("privacy.clearOnShutdown.cookies", true)
-user_pref("privacy.clearOnShutdown.sessions", true)
+user_pref("privacy.clearOnShutdown.cookies", false)
+user_pref("privacy.clearOnShutdown.sessions", false)
 user_pref("privacy.clearOnShutdown.cache", true)
 user_pref("privacy.clearOnShutdown.formdata", true)
 user_pref("privacy.clearOnShutdown.offlineApps", true)
