5dcdeeac18a817016d2967434064195e32eccbb5
Joao-O-Santos <joao.filip.oliv.santos94@gmail.com>
2026-04-28 02:40:41 +0100

    Update link_configs for bash

--- link_configs.sh
+++ link_configs.sh
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: CC0-1.0
-ln -s $XDG_CONFIG_HOME/shellrc $HOME/.profile
 ln -s $XDG_CONFIG_HOME/xorg/xinitrc $HOME/.xinitrc
+ln -s $XDG_CONFIG_HOME/shellrc $HOME/.profile
+# If using bash
+ln -s $XDG_CONFIG_HOME/shellrc $HOME/.bashrc
 #ln -s $XDG_CONFIG_HOME/firefox/profiles.ini $HOME/.mozilla/firefox/profiles.ini 
 #ln -s $XDG_CONFIG_HOME/firefox/hardened/user.js $HOME/.mozilla/firefox/hardened/user.js
 #ln -s $XDG_CONFIG_HOME/firefox/unsafe/user.js $HOME/.mozilla/firefox/unsafe/user.js
